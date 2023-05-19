008f30320b9c064df7334ec8bc00c7d29a63496b
mthodawu <mtho.dawu@gmail.com>
2023-05-19 12:13:27 +0200

    deleted:    .vscode/launch.json 	modified:   Level2.java

--- Level2.java
+++ Level2.java
@@ -68,6 +68,40 @@ public class Level2 {
         }
         return backpackCapacity;
     }
+
+    public static String maxValue(HashMap<String, Item> itemsHashMap, int capacity) {
+        // Create a list from the entries of the itemList
+        List<HashMap.Entry<String, Item>> itemList = new ArrayList<>(itemsHashMap.entrySet());
+        // System.out.println(itemList.toString()); // scaffolding
+        // Sort the items by value/weight ratio
+        Comparator<HashMap.Entry<String, Item>> valueComparator = Comparator
+                .comparing(entry -> entry.getValue().getValuePerWeight());
+        itemList.sort(valueComparator);
+
+        int backpackValue = 0;
+
+        // while there are still items in the list and the backpack isn't full
+        while (!itemList.isEmpty() && capacity > 0) {
+
+            Map.Entry<String, Item> entry = itemList.get(itemList.size() - 1);
+            Item item = entry.getValue(); // Access the Item object from the Map.Entry //get the item with the highest
+                                          // value/weight ratio
+
+            // System.out.println(entryToString(entry)); // scaffolding
+            if (item.getWeight() <= capacity) { // if the item fits in the backpack, add it to the backpack
+                // System.out.println(entryToString(entry) + " fits in the backpack");
+                capacity -= item.getWeight();
+                backpackValue += item.getValue();
+                itemList.remove(itemList.size() - 1);
+
+            }
+            // if the item doesn't fit in the backpack, remove it from the list
+            else {
+                itemList.remove(itemList.size() - 1);
+            }
+        }
+        return String.valueOf(backpackValue);
+    }
 }
 
 class Item {
